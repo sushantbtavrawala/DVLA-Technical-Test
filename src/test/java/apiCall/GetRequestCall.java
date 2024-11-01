95aa8bb7329c575ce655492de27a06820ed6d42b
sushant.tavrawala <106661829+sushanttavrawala@users.noreply.github.com>
2024-11-01 14:46:20 +0000

    Task 1 - Scenario 1,2, 3, 4 completed.

--- src/test/java/apiCall/GetRequestCall.java
+++ src/test/java/apiCall/GetRequestCall.java
@@ -3,7 +3,6 @@ package apiCall;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
 import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
--- src/test/java/commonUtils/CommonUtils.java
+++ src/test/java/commonUtils/CommonUtils.java
@@ -1,7 +1,5 @@
 package commonUtils;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
